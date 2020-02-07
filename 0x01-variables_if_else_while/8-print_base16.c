c6f85e6d9840b14efd3ba8da3f927674a14fb678
Phoebemat <phoebemattingly@gmail.com>
2020-02-07 02:42:43 +0000

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+	putchar(ch + '0');
+	}
+
+	for (ch = 'a' ; ch <= 'f' ; ch++);
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
