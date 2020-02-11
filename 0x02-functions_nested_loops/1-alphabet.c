93920a10058ceb4e1117a29be28e38a85c584572
Phoebemat <phoebemattingly@gmail.com>
2020-02-11 19:45:33 +0000

    edited header file, added function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include "holberton.h"
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+
+	_putchar(ch);
+
+	}
+
+	_putchar('\n');
+
+	return  (0);
+}
