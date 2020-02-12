35473b4508c5c2843bd680291e7f6afb620e1e69
Phoebemat <phoebemattingly@gmail.com>
2020-02-12 01:55:37 +0000

    simple addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * sum - add two int var together
+ * Return: sum of two int
+ * @a: first var
+ * @b: second var
+ */
+int sum(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -10,7 +10,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-int add(int, int);
+int sum(int, int);
 void times_table(void);
 void print_to_98(int n);
 
