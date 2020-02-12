cabbf1226ddb791bdd852f17134e28068a65d999
Phoebemat <phoebemattingly@gmail.com>
2020-02-12 02:18:24 +0000

    print time in analog

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints time in analog
+ * Return: set of sums in time format
+ */
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
