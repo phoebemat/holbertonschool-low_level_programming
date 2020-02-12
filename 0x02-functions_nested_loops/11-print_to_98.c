e89a205628f604f12c4c90edb35efa111b5b27f0
Phoebemat <phoebemattingly@gmail.com>
2020-02-12 20:45:51 +0000

    possibly complicated code

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_below_98 - integer to 98 printed
+ * @a: var type
+ *
+ * Return: return 1 through 98
+ */
+
+void print_to_98(int n)
+{
+	int m
+	for (n = m ; m < 99; m++)
+	{
+		if (m < 99)
+		putchar(m);
+		putchar(',');
+		putchar(' ');
+		else
+		putchar('\n');
+	}
+}
+	
