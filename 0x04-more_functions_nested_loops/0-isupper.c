d093db830fb2b6d9d80bc30583689c1670af469d
Phoebemat <phoebemattingly@gmail.com>
2020-02-13 15:26:12 +0000

    possibly angering the checker

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isupper - function to check for uppercase character
+ * @c: integer variable for function
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	int c;
+
+	while (_isupper(c))
+	{
+	return (1);
+	}
+
+}
