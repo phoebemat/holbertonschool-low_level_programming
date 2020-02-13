94a0027b0447628889cd85bc6ce52a9be358fb24
Phoebemat <phoebemattingly@gmail.com>
2020-02-13 15:56:22 +0000

    angering the checker

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	while (a < 10)
+	{
+	_putchar('a');
+	}
+
+	_putchar('\n');
+}
