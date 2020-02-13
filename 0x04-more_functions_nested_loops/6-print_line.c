008d3cf091a4ea15260d06b6001058d256dd5dd8
Phoebemat <phoebemattingly@gmail.com>
2020-02-13 21:55:23 +0000

    not even close

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,27 @@
-hello
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n);
+{
+	int n;
+
+	while (n)
+	{
+	_putchar('_');
+
+
+		if (n = 0)
+		{
+			_putchar('\n');
+		}
+
+
+
+	}
+
+}
