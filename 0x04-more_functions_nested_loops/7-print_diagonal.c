ab9d07bc467f6d21f7246bf39791eb52dae708d9
Phoebemat <phoebemattingly@gmail.com>
2020-02-14 04:01:51 +0000

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,2 +1,26 @@
-hello
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonal line based on n
+ * @n: integer for function
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	for (a = 1; a < n; a++)
+		for (b = 1; b < 1; b++)
+		{
+			_putchar(' ');
+			_putchar('\');
+			_putchar('\n');
+
+		}
+	_putchar('\n');
+
+
+}
 
