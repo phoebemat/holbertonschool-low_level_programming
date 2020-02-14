3f26b6f7c92ed6360a74faf079b372d25932ee61
Phoebemat <phoebemattingly@gmail.com>
2020-02-14 04:47:50 +0000

    first attempt

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	while (1 < 101)
+	{
+	if (a % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (a % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else if ((a % 3 == 0) && (a % 5 == 0))
+	{
+		printf("FizzBuzz ");
+	}
+	printf("%d ", a);
+	return (0);
+	}
+}
