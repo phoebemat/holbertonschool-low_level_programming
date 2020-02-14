96bfcfc44ffe4e120d5b7e378c981b2648bf1f11
Phoebemat <phoebemattingly@gmail.com>
2020-02-14 04:07:49 +0000

    moved nested braces

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,13 +12,13 @@ void print_diagonal(int n)
 	int b;
 
 	for (a = 1; a < n; a++)
+	{
 		for (b = 1; b < a; b++)
-		{
 			_putchar(' ');
+
 			_putchar('\\');
 			_putchar('\n');
-
-		}
+	}
 
 	if (n <= 0)
 	_putchar('\n');
