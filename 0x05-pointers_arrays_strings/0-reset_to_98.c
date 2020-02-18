c23d02b2573430532ec9cc19704516c7639b4c50
Phoebemat <phoebemattingly@gmail.com>
2020-02-18 14:54:55 +0000

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - update value to 98
+ *@n: integer variable
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme file for the project introducing us to pointers, arrays, and strings
