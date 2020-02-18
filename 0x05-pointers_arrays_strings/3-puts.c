de1fef04977563c06b1bcba5e5fb82db730c32c1
Phoebemat <phoebemattingly@gmail.com>
2020-02-18 19:11:50 +0000

    4th task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _puts - print string to stdout
+ * @str: string pointer
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+		{
+		_putchar(*str);
+		str++;
+		}
+
+	_putchar('\n');
+
+	return (0);
+}
