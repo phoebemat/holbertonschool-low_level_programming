ca0f752ca99ee15c33314aa65dca7b333857f7b3
Phoebemat <phoebemattingly@gmail.com>
2020-02-19 23:14:13 +0000

    removed weird characters

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-<<<<<<< HEAD
 #include <stdio.h>
 /**
  * print_array - print n elements of array
@@ -18,11 +17,3 @@ void print_array(int *a, int n)
 	printf("\n");
 	}
 }
-=======
-
-/**
- * 
- *
- * Return: Always 0.
- */
->>>>>>> e0075d0fa398384102177f2c87d082e4e8139d55
